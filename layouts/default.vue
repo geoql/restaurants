7dce9e91f229ebdb543a620c82c9f78b691e9648
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-12-17 01:55:21 +0530

    fix: use `@nuxtjs/composition-api`

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent } from '@vue/composition-api';
+  import { defineComponent } from '@nuxtjs/composition-api';
   import VHeader from '@/components/VHeader.vue';
   import VFooter from '@/components/VFooter.vue';
 
