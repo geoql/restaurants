48691e070f139de8957355bb3ffd7f9b00cb918b
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2021-03-12 12:02:04 +0530

    fix: issue caused by eslint config prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     '@nuxtjs',
     '@nuxtjs/eslint-config-typescript',
     'prettier',
-    'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
