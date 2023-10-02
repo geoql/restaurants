7d9b11ee46f25976876837d76e5c2baee970063b
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2023-10-02 10:39:23 +0530

    chore(analytics): fix typo in domain
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- config/modules/plausible.ts
+++ config/modules/plausible.ts
@@ -3,7 +3,7 @@ import type { NuxtConfig } from 'nuxt/schema';
 export const config: NuxtConfig['plausible'] = {
   hashMode: false,
   trackLocalhost: false,
-  domain: 'restuarants.geoql.in',
+  domain: 'restaurants.geoql.in',
   apiHost: 'https://analytics.geoql.in',
   autoPageviews: true,
   autoOutboundTracking: true,
